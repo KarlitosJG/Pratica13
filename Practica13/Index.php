430203e5974f4de36d29a577cf40826fac57d6ad
Carlos Daniel JG <30677816+KarlitosJG@users.noreply.github.com>
2017-10-11 23:19:45 -0500

    Add files via upload

---
+++ Practica13/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="Procesar.php" method="post" name="DatosPersonales">
+  Nombre:<br>
+  <input type="text" name="NombreAlumno" value="" placeholder="Introduce tu Nombre">
+  <br>
+  Apellidos:<br>
+  <input type="text" name="ApellidosAlumno" value="" placeholder="Introduce tu Apellido">
+  <br>
+  Edad:<br>
+  <input type="text" name="EdadAlumno" value="" placeholder="Introduce tu Edad">
+  <br><br>
+  <input type="submit" value="Guardar">
+</form> 
+
+<p>Carlos Daniel Jimenez Gomez</p>
+
+</body>
+</html>
